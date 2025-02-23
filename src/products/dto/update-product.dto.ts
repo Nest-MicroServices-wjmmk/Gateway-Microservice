1b8bf7cfb5e10929e91c2935fbd415af6e99f241
William Mosquera <wjmmk3@gmail.com>
2025-02-23 15:25:38 -0500

    feat: product-dto updated

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,6 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
-import { IsInt, IsNumber, IsPositive } from 'class-validator';
-import { Type } from 'class-transformer';
 
 export class UpdateProductDto extends PartialType(CreateProductDto) {}
