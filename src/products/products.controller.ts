edb5f73c34d1e956ec55cb066900031ae816d630
William Mosquera <wjmmk3@gmail.com>
2025-03-04 11:46:27 -0500

    fix: product-module in gateway updated

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,20 +1,20 @@
 import { BadRequestException, Body, Controller, Delete, Get, Inject, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
-import { ClientGrpcProxy, RpcException } from '@nestjs/microservices';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError, firstValueFrom } from 'rxjs';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
-import { PRODUCT_SERVICE } from 'src/config';
+import { NATS_SERVICE } from 'src/config';
 import { CreateProductDto, UpdateProductDto } from './dto';
 
 @Controller('products')
 export class ProductsController {
   constructor(
-    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientGrpcProxy
+    @Inject(NATS_SERVICE) private readonly client: ClientProxy
   ) {}
   
   @Post()
   async createPoduct(@Body() createProductDto: CreateProductDto) {
     const product = await firstValueFrom(
-      this.productsClient.send({ cmd: 'create_product' }, createProductDto)
+      this.client.send({ cmd: 'create_product' }, createProductDto)
       );
     return product;
   }
@@ -22,7 +22,7 @@ export class ProductsController {
   @Get()
   findAllProducts(@Query() paginationDto: PaginationDto) {
     try {
-      return this.productsClient.send({ cmd: 'findAll_products' }, paginationDto);
+      return this.client.send({ cmd: 'findAll_products' }, paginationDto);
     } catch (error) {
       throw new BadRequestException(error.message);
     }
@@ -31,7 +31,7 @@ export class ProductsController {
   @Get(':id')
   async finOneProduct(@Param('id', ParseIntPipe) id: number) {
     try {
-      const product = await firstValueFrom(this.productsClient.send({ cmd: 'findOne_product' }, { id }));
+      const product = await firstValueFrom(this.client.send({ cmd: 'findOne_product' }, { id }));
       return product;
     } catch (err) {
       throw new RpcException(err.message);
@@ -42,7 +42,7 @@ export class ProductsController {
   @Patch(':id')
   async updateProduct(@Param('id', ParseIntPipe) id: number, @Body() updateProductDto: UpdateProductDto) {
     const product = await firstValueFrom(
-      this.productsClient.send({ cmd: 'update_product' }, { id, ...updateProductDto })
+      this.client.send({ cmd: 'update_product' }, { id, ...updateProductDto })
       .pipe( catchError( err => { throw new RpcException(err)} ))
     );
     return product;
@@ -51,7 +51,7 @@ export class ProductsController {
   @Delete(':id')
   async deleteProduct(@Param('id', ParseIntPipe) id: number) {
     const product = await firstValueFrom(
-      this.productsClient.send({ cmd: 'delete_product' }, { id })
+      this.client.send({ cmd: 'delete_product' }, { id })
       .pipe( catchError( err => { throw new RpcException(err)} ))
     );
     return product;
